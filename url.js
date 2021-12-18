12f8f4d134c844b64443d20d88614df76443cd84
emma415661 <96326808+emma415661@users.noreply.github.com>
2021-12-18 12:55:05 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://forms.gle/VPycULmqAaUCJ4ar7',
+	'https://forms.gle/NphKR1yAC6CzeEKn9'
 ];
