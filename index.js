92c1be8e3db56292d8ccb56fdb2f98fbad8274b3
emma415661 <96326808+emma415661@users.noreply.github.com>
2022-01-29 23:06:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-window.location.href = urls.length > 0 ? urls[Math.floor(Math.random() * urls.length)] : 'https://emma415661.github.io/random-survey/';
+window.location.href = urls.length > 0 ? urls[Math.floor(Math.random() * urls.length)] : 'https://survey123.github.io/random-survey/';
